135b1ab01d7e582de28c2a6ee91bc2821d79f694
babuharsh19 <babuharsh19@gmail.com>
2022-02-17 16:28:42 +0530

    Added solution - LeetHub

--- 8. Exactly 3 Divisors - GFG/8.-exactly-3-divisors.cpp
+++ 8. Exactly 3 Divisors - GFG/8.-exactly-3-divisors.cpp
@@ -16,7 +16,7 @@ class Solution{
         if(n%2==0||n%3==0) return false;
         for(int i=5;i*i<=n;i=i+6)
         {
-            if((n%i==0||n%(i+2)==0)){
+            if((n%i==0||n%(i+2)==0)&n!=(i+2)){
                 return false;
             }
         }
