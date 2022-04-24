36d94b603e7268cc726e1f2353687ab2a89b463b
Harsh Babu <babuharsh19@gmail.com>
2022-04-24 08:54:40 +0530

    Time: 22 ms, Memory: 18 MB - LeetHub

---
+++ missing-number/missing-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int x=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            x = x^i^nums[i];
+        }
+        return x^0^(nums.size());
+    }
+};
