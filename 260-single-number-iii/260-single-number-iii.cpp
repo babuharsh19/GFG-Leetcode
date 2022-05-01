bc850675b6a3aa2bd3518bf2edadb9e84b573b08
Harsh Babu <babuharsh19@gmail.com>
2022-05-01 09:57:18 +0530

    Time: 8 ms (84.35%), Space: 10.1 MB (41.94%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        long long int xor1=0;
+            int k=0,n=nums.size(),x=0;
+        if(n<=2) return nums;
+        vector<int>res(2);
+        for(int i=0;i<n;i++)
+        {
+            xor1=xor1^nums[i];
+        }
+        xor1=xor1&~(xor1-1);//finding right most set bit of xor
+        for(int i=0;i<n;i++)
+        {
+            if(xor1&nums[i])
+            {
+                x=x^nums[i];
+            }
+            else
+            {
+                k=k^nums[i];
+            }
+        }
+        res[0]=x;
+        res[1]=k;
+        return res;
+    }
+};
