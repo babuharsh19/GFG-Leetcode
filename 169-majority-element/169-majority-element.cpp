b272e219b32696b24cef5d4af0505eb00c657b5d
Harsh Babu <babuharsh19@gmail.com>
2022-05-08 23:49:23 +0530

    Time: 25 ms (54.51%), Space: 19.5 MB (59.88%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
+    //naive approach
+    // int majorityElement(vector<int>& nums) {
+    //     int count=1,element=nums[0];
+    //     sort(nums.begin(),nums.end());
+    //     for(int i=1;i<nums.size();i++)
+    //     {
+    //        if(nums[i]==nums[i-1]){
+    //            count++;
+    //            element=nums[i];
+    //            if(count>(nums.size()/2))return element;
+    //        } 
+    //         else{
+    //             count=1;
+    //             element=nums[i];
+    //         }
+    //     }
+    //     return element;
+    // }
+    //optimize approach using more's voting algorithm
+    
     int majorityElement(vector<int>& nums) {
-        int count=1,element=nums[0];
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++)
+        int count=1,n=nums.size(),element=nums[0];
+        for(int i=1;i<n;i++)
         {
-           if(nums[i]==nums[i-1]){
-               count++;
-               element=nums[i];
-               if(count>(nums.size()/2))return element;
-           } 
-            else{
+            if(nums[i]==element)
+            {count++;}
+            else{count--;}
+            if(count==0)
+            {
                 count=1;
                 element=nums[i];
             }
