b524fb2261d78b5531ff6d3627ad8aaa4b654392
Harsh Babu <babuharsh19@gmail.com>
2022-06-02 09:39:03 +0530

    Time: 21 ms (43.41%), Space: 13.3 MB (19.38%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        unordered_map<int,int>m;
+        for(int i=0;i<matrix.size();i++){
+            bool k=false;
+            for(int j=0;j<matrix[0].size();j++){
+                if(matrix[i][j]==0){
+                    k =true;
+                    m[j]=i;
+                }
+                if((k==true)&&j==matrix[i].size()-1){
+                    matrix[i]=vector<int>(matrix[i].size(),0);
+                }
+            // k=false;
+            }
+        }
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                auto it=m.find(j);
+                if(it!=m.end())matrix[i][j]=0;
+            }
+        }
+    }
+};
